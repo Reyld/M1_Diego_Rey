d13fe09ea81502d899fa486ac5b3a16d09b95261
Reyld <77274308+Reyld@users.noreply.github.com>
2021-01-14 10:51:06 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,23 @@ setInterval('allrun()',1);
     else if ((document.getElementById("prod3").value)<0){
         ((document.getElementById("prod3").value)=0);
     }
+
+}
+
+function allrun2(){
+    setInterval('allrun2()', 1);
+
+    if ((document.getElementById("prodedit").value)<0){
+        ((document.getElementById("prodedit").value)=0);
+    }
+
+    else if ((document.getElementById("prodadd").value)<0){
+        ((document.getElementById("prodadd").value)=0);
+    }
+    
+    else if ((document.getElementById("price").value)<0){
+        ((document.getElementById("price").value)=0);
+    }
 }
 
 
@@ -34,30 +51,6 @@ function calculo(){
     var total = parseInt(((document.getElementById("prod1").value)*precio1)+((document.getElementById("prod2").value)*precio2)+((document.getElementById("prod3").value)*precio3));
 
     document.getElementById("Resultado").innerHTML=total;
-
-    /*
-
-    if ((document.getElementById("prod1").value)>5){
-        ((document.getElementById("prod1").value)=5);
-    }
-    else if ((document.getElementById("prod1").value)<0){
-        ((document.getElementById("prod1").value)=0);
-    }
-    else if ((document.getElementById("prod2").value)>2){
-        ((document.getElementById("prod2").value)=8);
-    }
-    else if ((document.getElementById("prod2").value)<0){
-        ((document.getElementById("prod2").value)=0);
-    }
-    else if ((document.getElementById("prod3").value)>2){
-        ((document.getElementById("prod3").value)=2);
-    }
-    else if ((document.getElementById("prod3").value)<0){
-        ((document.getElementById("prod3").value)=0);
-    }
-
-    */
-
 
 }
 
@@ -72,7 +65,7 @@ function pedir(){
 // Botón quitafilas
 $(function () {
 
-    $(".buttonborra").click(function () {
+    $(".btn-danger").click(function () {
         if (confirm("El producto se eliminará. ¿Continuar?")) {
         //    $(this).parent().parent().hide();
             $(this).parent().parent().remove();     // Provisional
